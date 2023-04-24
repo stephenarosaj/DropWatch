ce92f57e64379b93b98ac1fe5166ebf7ed62b36a
Bryanna <bryanna@bryannas-mbp.devices.brown.edu>
2023-04-24 16:48:35 -0400

    frontend: started authorization

--- frontend/src/authorization.js
+++ frontend/src/authorization.js
@@ -8,9 +8,9 @@ function generateRandomString(length) {
   return text;
 }
 
-const digest = await window.crypto.subtle.digest("SHA-256", data);
+//export const digest = await window.crypto.subtle.digest("SHA-256", data);
 
-async function generateCodeChallenge(codeVerifier) {
+export async function generateCodeChallenge(codeVerifier) {
   function base64encode(string) {
     return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
       .replace(/\+/g, "-")
@@ -25,69 +25,76 @@ async function generateCodeChallenge(codeVerifier) {
   return base64encode(digest);
 }
 
-const clientId = "secret";
-const redirectUri = "http://localhost:3232";
-
 let codeVerifier = generateRandomString(128);
 
-generateCodeChallenge(codeVerifier).then((codeChallenge) => {
-  let state = generateRandomString(16);
-  let scope = "user-read-private user-read-email";
-
-  localStorage.setItem("code_verifier", codeVerifier);
-
-  let args = new URLSearchParams({
-    response_type: "code",
-    client_id: clientId,
-    scope: scope,
-    redirect_uri: redirectUri,
-    state: state,
-    code_challenge_method: "S256",
-    code_challenge: codeChallenge,
-  });
-
-  window.location = "https://accounts.spotify.com/authorize?" + args;
-});
 
 const urlParams = new URLSearchParams(window.location.search);
 let code = urlParams.get("code");
 
-let body = new URLSearchParams({
-  grant_type: "authorization_code",
-  code: code,
-  redirect_uri: redirectUri,
-  client_id: clientId,
-  code_verifier: codeVerifier,
+
+
+// let body = new URLSearchParams({
+//   grant_type: "authorization_code",
+//   code: code,
+//   redirect_uri: redirectUri,
+//   client_id: clientId,
+//   code_verifier: codeVerifier,
+// });
+
+// const response = fetch("https://accounts.spotify.com/api/token", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/x-www-form-urlencoded",
+//   },
+//   body: body,
+// })
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("HTTP status " + response.status);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     localStorage.setItem("access_token", data.access_token);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+// async function getProfile(accessToken) {
+//   let access_Token = localStorage.getItem("access_token");
+
+//   const response = await fetch("https://api.spotify.com/v1/me", {
+//     headers: {
+//       Authorization: "Bearer " + access_Token,
+//     },
+//   });
+
+//   const data = await response.json();
+// }
+
+export function requestAuthorization(client_id, redirect_uri) {
+  const clientId = client_id;
+  const redirectUri = redirect_uri;
+
+let codeVerifier = generateRandomString(128);
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+  let state = generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
+
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
 });
-
-const response = fetch("https://accounts.spotify.com/api/token", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  body: body,
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    localStorage.setItem("access_token", data.access_token);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
-
-async function getProfile(accessToken) {
-  let accessToken = localStorage.getItem("access_token");
-
-  const response = await fetch("https://api.spotify.com/v1/me", {
-    headers: {
-      Authorization: "Bearer " + accessToken,
-    },
-  });
-
-  const data = await response.json();
 }
---
+++ frontend/src/private/tokens.js
@@ -0,0 +1,2 @@
+export const clientID = "62ab84fad1d74a4b9faab020e4fbd210"
+export const clientSecretID = "dd09f5fa3d114571b70fd4e70d2ee260"
