4fc334bda8f2837b25034912c4fe94e95080663e
Maia Mongado <maiamongado@maias-mbp.devices.brown.edu>
2023-05-07 16:54:50 -0400

    trying to make user data endpoint on backend

--- frontend/src/functions/followed.js
+++ frontend/src/functions/followed.js
@@ -1,23 +1,24 @@
 export async function getFollowed(setFollowed) {
-  let access_token = localStorage.getItem('access_token')
-  let headers = {headers : {Authorization: 'Bearer ' + access_token}}
-  fetch('https://api.spotify.com/v1/me/following?type=artist&limit=10', headers)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error('HTTP status' + response.status);
+  let access_token = localStorage.getItem("access_token");
+  // let headers = { headers: { Authorization: "Bearer " + access_token } };
+  // fetch("https://api.spotify.com/v1/me/following?type=artist&limit=10", headers)
+  fetch("http://localhost:3232/user_data?user_token=" + access_token)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP status" + response.status);
       }
       return response.json();
     })
-      .then(data => {
-        let artists = data.artists
-        if(artists !== undefined) {
-          console.log(artists)
-          if(artists.items !== undefined) {
-            console.log(artists.items)
-            setFollowed(artists.items)
-          }
+    .then((data) => {
+      let artists = data.artists;
+      if (artists !== undefined) {
+        console.log(artists);
+        if (artists.items !== undefined) {
+          console.log(artists.items);
+          setFollowed(artists.items);
         }
-      })
+      }
+    });
 }
 
 // export async function getUserInfo(query, setStateVar) {
